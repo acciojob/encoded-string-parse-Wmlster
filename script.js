d472d3b61790bc6d511187313cc8a3fca92a2500
Manik Sharma <90952302+Wmlster@users.noreply.github.com>
2023-11-23 16:39:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,70 @@
-const parseCode = (str) => {
+const parseCode = (s) => {
   // your code here
+
+	let obj={};
+
+	let n=s.length;
+
+	let j=0;
+	let name="";
+	
+	for(let i=0;i<n;i++)
+		{
+			if(s[i]=='0')
+			{
+				j=i;
+				break;
+			}
+
+			name+=s[i];
+		}
+
+	obj["firstName"]=name;
+
+	for(let i=j;i<n;i++)
+		{
+			if(s[i]!='0')
+			{
+				j=i;
+				break;
+			}
+		}
+
+	let lname="";
+
+	for(let i=j;i<n;i++)
+		{
+			if(s[i]=='0')
+			{
+				j=i;
+				break;
+			}
+
+			lname+=s[i];
+		}
+
+	obj["lastName"]=lname;
+
+	for(let i=j;i<n;i++)
+		{
+			if(s[i]!='0')
+			{
+				j=i;
+				break;
+			}
+		}
+
+	let id="";
+
+	for(let i=j;i<n;i++)   
+		{
+			id+=s[i];
+		}
+
+	obj["id"]=id;
+
+	return obj;
+	
 };
 
 // Do not change the code below
